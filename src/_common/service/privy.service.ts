83bc0c959a2929eefead8e936f63e78990c96e89
mohitM0 <mohit@m0.ventures>
2025-04-08 14:33:31 +0000

    feat: add PrivyConfig service for initializing PrivyClient with project-specific credentials

---
+++ src/_common/service/privy.service.ts
@@ -0,0 +1,26 @@
+import { PrivyClient } from '@privy-io/server-auth';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+export type ProjectType = 'invoice' | 'otc' | 'seekers';
+
+@Injectable()
+export class PrivyConfig {
+  constructor(private configService: ConfigService) {}
+
+  initializePrivyClient(projectType: ProjectType): PrivyClient {
+    const appIdKey = `PRIVY_APP_ID_${projectType.toUpperCase()}`;
+    const appSecretKey = `PRIVY_APP_SECRET_${projectType.toUpperCase()}`;
+    const authKeyKey = `PRIVY_AUTHORIZATION_PRIVATE_KEY_${projectType.toUpperCase()}`;
+
+    const appId = this.configService.getOrThrow<string>(appIdKey);
+    const appSecret = this.configService.getOrThrow<string>(appSecretKey);
+    const authorizationPrivateKey = this.configService.getOrThrow<string>(authKeyKey);
+
+    return new PrivyClient(appId, appSecret, {
+      walletApi: {
+        authorizationPrivateKey,
+      },
+    });
+  }
+}
